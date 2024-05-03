38fb657d9d321c4dfc8aec7ceafc312bf91c1e20
Kristian Quirapas <kmquirapas@gmail.com>
2024-05-03 19:56:29 +0800

    add: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "KEEBY",
   description: "Shipping between infra and user",
+  icons: {
+    icon: "/assets/keeby.png",
+  },
 };
 
 export default function RootLayout({
