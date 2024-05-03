8e72122bd7733e1feea48a2de0a91583f71834ad
Kristian Quirapas <kmquirapas@gmail.com>
2024-05-03 19:54:08 +0800

    add: initial website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "keeby-orange": "#ff5800",
+        "keeby-white": "#ffffff",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
