1e896c54dcab32ea62ab07e38063def8c5f67857
Kristian Quirapas <kmquirapas@gmail.com>
2024-05-09 18:55:25 +0800

    add: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "KEEBY",
   description: "Shipping between infra and user",
+  icons: {
+    icon: "./icon.ico",
+    shortcut: "./icon.ico",
+    apple: "./apple-icon.png",
+  },
 };
 
 export default function RootLayout({
